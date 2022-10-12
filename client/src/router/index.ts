0604efb43984355d02743e57b1c315d76e345d28
unknown <dkpark10@naver.com>
2022-10-12 18:27:05 +0900

    [update]코드스플리팅으로 번들 일부 최적화

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import {
   createRouter,
   createWebHistory,
@@ -10,38 +11,38 @@ import RecentReadPost from "../components/page/RecentReadPost.vue";
 import PostDetail from "../components/page/PostDetail.vue";
 import Hub from "../components/page/Hub.vue";
 
-const notcsRouter = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/login",
-    name: "login",
-    component: Login,
-  },
-  {
-    path: "/ranking",
-    name: "ranking",
-    component: Ranking,
-  },
-  {
-    path: "/recent",
-    name: "recent-read-posts",
-    component: RecentReadPost,
-  },
-  {
-    path: "/post/:id",
-    name: "post-detail",
-    component: PostDetail,
-  },
-  {
-    path: "/:category",
-    name: "category",
-    component: Hub,
-  },
-];
+// const notcsRouter = [
+//   {
+//     path: "/",
+//     name: "home",
+//     component: HomeView,
+//   },
+//   {
+//     path: "/login",
+//     name: "login",
+//     component: Login,
+//   },
+//   {
+//     path: "/ranking",
+//     name: "ranking",
+//     component: Ranking,
+//   },
+//   {
+//     path: "/recent",
+//     name: "recent-read-posts",
+//     component: RecentReadPost,
+//   },
+//   {
+//     path: "/post/:id",
+//     name: "post-detail",
+//     component: PostDetail,
+//   },
+//   {
+//     path: "/:category",
+//     name: "category",
+//     component: Hub,
+//   },
+// ];
 
 export default () => {
   const isSsrMode = typeof window === "undefined";
@@ -65,25 +66,19 @@ export default () => {
         path: "/ranking",
         name: "ranking",
         component: () =>
-          import(
-            /* webpackChunkName: ranking */ "../components/page/Ranking.vue"
-          ),
+          import(/* webpackChunkName: ranking */ "../components/page/Ranking.vue"),
       },
       {
         path: "/recent",
         name: "recent-read-posts",
         component: () =>
-          import(
-            /* webpackChunkName: recentreadpost */ "../components/page/RecentReadPost.vue"
-          ),
+          import(/* webpackChunkName: recentreadpost */ "../components/page/RecentReadPost.vue"),
       },
       {
         path: "/post/:id",
         name: "post-detail",
         component: () =>
-          import(
-            /* webpackChunkName: postdetail */ "../components/page/PostDetail.vue"
-          ),
+          import(/* webpackChunkName: postdetail */ "../components/page/PostDetail.vue"),
       },
       {
         path: "/:category",
